d7c869fac2f373242da90acbed42e52ab9909e8e
Noam Almog <noama@wix.com>
2014-10-19 12:20:31 +0300

    set more lenient download timeout values

--- src/main/java/com/wix/mysql/config/DownloadConfigBuilder.java
+++ src/main/java/com/wix/mysql/config/DownloadConfigBuilder.java
@@ -2,6 +2,7 @@ package com.wix.mysql.config;
 
 import com.wix.mysql.PackagePaths;
 import de.flapdoodle.embed.process.config.store.DownloadPath;
+import de.flapdoodle.embed.process.config.store.TimeoutConfigBuilder;
 import de.flapdoodle.embed.process.distribution.Platform;
 import de.flapdoodle.embed.process.extract.UUIDTempNaming;
 import de.flapdoodle.embed.process.io.directories.UserHome;
@@ -22,6 +23,7 @@ public class DownloadConfigBuilder extends de.flapdoodle.embed.process.config.st
         downloadPrefix().setDefault(new DownloadPrefix("embedmysql-download"));
         userAgent().setDefault(new UserAgent("Mozilla/5.0 (compatible; Embedded MySql; +https://github.com/wix/wix-embedded-mysql)"));
         packageResolver().setDefault(new PackagePaths());
+        timeoutConfig().setDefault(new TimeoutConfigBuilder().connectionTimeout(10000).readTimeout(60000).build());
         return this;
     }
 }
