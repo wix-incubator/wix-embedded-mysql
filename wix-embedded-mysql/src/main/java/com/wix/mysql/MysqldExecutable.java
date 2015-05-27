1dba317f0110c681fb0e2fca2f88e4e9dc59f6ae
Vilius Lukosius <vilius@wix.com>
2015-05-27 11:38:57 +0300

    Updates:  - upgraded flapdoodle.process version;  - got rid of jul;  - clean-up output logs;

--- wix-embedded-mysql/src/main/java/com/wix/mysql/MysqldExecutable.java
+++ wix-embedded-mysql/src/main/java/com/wix/mysql/MysqldExecutable.java
@@ -2,20 +2,20 @@ package com.wix.mysql;
 
 import com.wix.mysql.config.MysqldConfig;
 import com.wix.mysql.exceptions.MissingDependencyException;
-import com.wix.mysql.input.CollectingLogOutputProcessor;
 import de.flapdoodle.embed.process.config.IRuntimeConfig;
 import de.flapdoodle.embed.process.config.store.FileType;
 import de.flapdoodle.embed.process.distribution.Distribution;
 import de.flapdoodle.embed.process.distribution.Platform;
 import de.flapdoodle.embed.process.extract.IExtractedFileSet;
-import de.flapdoodle.embed.process.io.Processors;
 import de.flapdoodle.embed.process.runtime.Executable;
+import org.apache.commons.io.IOUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.File;
 import java.io.IOException;
-import java.io.InputStreamReader;
+
+import static java.lang.String.format;
 
 /**
  * @author viliusl
@@ -23,10 +23,7 @@ import java.io.InputStreamReader;
  */
 public class MysqldExecutable extends Executable<MysqldConfig, MysqldProcess> {
 
-    private final Logger log = LoggerFactory.getLogger(getClass().getName());
-
     private final IExtractedFileSet executable;
-    private final IRuntimeConfig runtimeConfig;
 
     public MysqldExecutable(
             final Distribution distribution,
@@ -35,7 +32,6 @@ public class MysqldExecutable extends Executable<MysqldConfig, MysqldProcess> {
             final IExtractedFileSet executable) {
         super(distribution, config, runtimeConfig, executable);
         this.executable = executable;
-        this.runtimeConfig = runtimeConfig;
     }
 
     @Override
@@ -60,24 +56,18 @@ public class MysqldExecutable extends Executable<MysqldConfig, MysqldProcess> {
 
     private void initDatabase() throws IOException {
         try {
-            String baseDir = this.executable.generatedBaseDir().getAbsolutePath();
-            CollectingLogOutputProcessor logTo = new CollectingLogOutputProcessor(log);
-
             Process p = Runtime.getRuntime().exec(new String[]{
                             "scripts/mysql_install_db",
                             "--no-defaults",
-                            String.format("--basedir=%s", baseDir),
-                            String.format("--datadir=%s/data", baseDir)},
+                            format("--basedir=%s", getBaseDir()),
+                            format("--datadir=%s/data", getBaseDir())},
                     null,
-                    this.executable.generatedBaseDir());
-
-            Processors.connect(new InputStreamReader(p.getInputStream()), logTo);
-            Processors.connect(new InputStreamReader(p.getErrorStream()), logTo);
+                    getBaseDir());
 
             int retCode = p.waitFor();
 
             if (retCode != 0) {
-                resolveException(retCode, logTo.getOuptut());
+                resolveException(retCode, IOUtils.toString(p.getInputStream()) + IOUtils.toString(p.getErrorStream()));
             }
 
         } catch (InterruptedException e) {
@@ -92,10 +82,9 @@ public class MysqldExecutable extends Executable<MysqldConfig, MysqldProcess> {
                     "Please install it via system package manager, ex. 'sudo apt-get install libaio1'.\n" +
                     "For details see: http://bugs.mysql.com/bug.php?id=60544");
         } else {
-            throw new RuntimeException(String.format("'scripts/mysql_install_db' command exited with error code: %s", retCode));
+            throw new RuntimeException(format("'scripts/mysql_install_db' command exited with error code: '%s' and output: '%s'", retCode, output));
         }
     }
 
     protected File getBaseDir() { return this.executable.generatedBaseDir(); }
-    protected IRuntimeConfig getRuntimeConfig() { return this.runtimeConfig; }
 }
--- wix-embedded-mysql/src/test/scala/com/wix/mysql/IntegrationTest.scala
+++ wix-embedded-mysql/src/test/scala/com/wix/mysql/IntegrationTest.scala
@@ -5,12 +5,9 @@ import ch.qos.logback.classic.spi.ILoggingEvent
 import ch.qos.logback.classic.{Logger, LoggerContext}
 import ch.qos.logback.core.read.ListAppender
 import com.mchange.v2.c3p0.ComboPooledDataSource
-import com.wix.mysql.IntegrationTest._
 import com.wix.mysql.config.MysqldConfig
 import org.slf4j.LoggerFactory
-import org.slf4j.bridge.SLF4JBridgeHandler
 import org.specs2.mutable.SpecWithJUnit
-import org.specs2.specification.BeforeAll
 import org.springframework.jdbc.core.JdbcTemplate
 
 import scala.collection.JavaConversions._
@@ -19,10 +16,8 @@ import scala.collection.JavaConversions._
  * @author viliusl
  * @since 27/03/15
  */
-class IntegrationTest extends SpecWithJUnit with BeforeAll {
+class IntegrationTest extends SpecWithJUnit {
   sequential
-
-  override def beforeAll: Unit = init
 
   def givenMySqlWithConfig(config: MysqldConfig) = MysqldStarter.defaultInstance.prepare(config)
 
@@ -69,16 +64,5 @@ class IntegrationTest extends SpecWithJUnit with BeforeAll {
     new Iterable[String] {
       def iterator = appender.list map ( _.getMessage ) iterator
     }
-  }
-
-}
-
-object IntegrationTest {
-  lazy val init = {
-    import java.util.logging.{Level, LogManager, Logger}
-
-    LogManager.getLogManager.reset
-    SLF4JBridgeHandler.install
-    Logger.getLogger("global").setLevel(Level.FINEST)
   }
 }
