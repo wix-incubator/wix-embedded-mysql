18431797255828c32a27c801692c54958c587476
Vilius Lukosius <vilius@wix.com>
2015-06-07 02:48:19 +0300

    Implemented schema config

---
+++ wix-embedded-mysql/src/test/scala/com/wix/mysql/v3/SchemaConfigTest.scala
@@ -0,0 +1,52 @@
+package com.wix.mysql.v3
+
+import java.io.File
+
+import com.wix.mysql.config.{Charset, SchemaConfig}
+import org.specs2.mutable.SpecWithJUnit
+import scala.collection.convert.decorateAsScala._
+
+/**
+ * @author viliusl
+ * @since 06/06/15
+ */
+class SchemaConfigTest extends SpecWithJUnit {
+
+  "SchemaConfig" should {
+    "provide defaults" in {
+      val schemaConfig = SchemaConfig.defaults("aschema")
+
+      schemaConfig.getName mustEqual "aschema"
+      schemaConfig.getCharset mustEqual Charset.defaults
+      schemaConfig.getScripts.asScala must beEmpty
+    }
+
+    "build with defaults" in {
+      val schemaConfig = SchemaConfig.Builder("aschema").build
+
+      schemaConfig.getName mustEqual "aschema"
+      schemaConfig.getCharset mustEqual Charset.defaults
+      schemaConfig.getScripts.asScala must beEmpty
+    }
+
+    "build with custom charset" in {
+      val charset = Charset.of("charset", "collate")
+
+      val schemaConfig = SchemaConfig.Builder("aschema")
+        .withCharset(charset)
+        .build
+
+      schemaConfig.getCharset mustEqual charset
+    }
+
+    "build with Files" in {
+      val files = Seq(new File("/some"), new File("/some/other"))
+
+      val schemaConfig = SchemaConfig.Builder("aschema")
+        .withScripts(files:_*)
+        .build()
+
+      schemaConfig.getScripts.asScala mustEqual files
+    }
+  }
+}
