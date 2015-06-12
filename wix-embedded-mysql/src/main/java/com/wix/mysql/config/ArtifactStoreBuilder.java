44a302a7595c45e6eca26a640621a1e49a5e0600
Vilius Lukosius <vilius@wix.com>
2015-06-12 14:56:16 +0300

    Added poor mans copier of preextracted files.

--- wix-embedded-mysql/src/main/java/com/wix/mysql/config/ArtifactStoreBuilder.java
+++ wix-embedded-mysql/src/main/java/com/wix/mysql/config/ArtifactStoreBuilder.java
@@ -1,21 +1,24 @@
 package com.wix.mysql.config;
 
-import com.wix.mysql.ExtractingDownloader;
+import com.google.common.io.Files;
 import com.wix.mysql.config.directories.TargetGeneratedFixedPath;
 import com.wix.mysql.config.extract.NopNaming;
-import de.flapdoodle.embed.process.builder.TypedProperty;
+import de.flapdoodle.embed.process.config.store.FileSet;
 import de.flapdoodle.embed.process.config.store.IDownloadConfig;
-import de.flapdoodle.embed.process.config.store.ILibraryStore;
+import de.flapdoodle.embed.process.config.store.IPackageResolver;
 import de.flapdoodle.embed.process.distribution.Distribution;
-import de.flapdoodle.embed.process.extract.IExtractedFileSet;
-import de.flapdoodle.embed.process.extract.ITempNaming;
+import de.flapdoodle.embed.process.extract.*;
 import de.flapdoodle.embed.process.io.directories.IDirectory;
 import de.flapdoodle.embed.process.store.*;
+import org.apache.commons.io.filefilter.DirectoryFileFilter;
 import org.rauschig.jarchivelib.ArchiveFormat;
 import org.rauschig.jarchivelib.ArchiverFactory;
 import org.rauschig.jarchivelib.CompressionType;
+import static de.flapdoodle.embed.process.config.store.FileType.Executable;
+import static de.flapdoodle.embed.process.config.store.FileType.Library;
 
 import java.io.File;
+import java.io.FileFilter;
 import java.io.IOException;
 
 
@@ -49,17 +52,36 @@ public class ArtifactStoreBuilder extends de.flapdoodle.embed.process.store.Arti
     public IArtifactStore build() {
         Object artifactStore = new MyArtifactStore(downloadConfig, tempDir, executableNaming, downloader);
         return (IArtifactStore)artifactStore;
-
     }
 
     public static class MyArtifactStore extends ArtifactStore {
 
         private IDownloadConfig downloadConfig;
+        private IDirectory tempDirFactory;
 
         public MyArtifactStore(IDownloadConfig downloadConfig, IDirectory tempDirFactory, ITempNaming executableNaming, IDownloader downloader) {
             super(downloadConfig, tempDirFactory, executableNaming, downloader);
             this.downloadConfig = downloadConfig;
+            this.tempDirFactory = tempDirFactory;
         }
+
+        FileSet filesToExtract = FileSet.builder()
+        .addEntry(Executable, "bin/mysqld")
+        .addEntry(Library,    "bin/mysql")
+        .addEntry(Library,    "bin/resolveip")
+        .addEntry(Library,    "bin/mysqladmin")
+        .addEntry(Library,    "bin/my_print_defaults")
+        .addEntry(Library,    "scripts/mysql_install_db")
+        .addEntry(Library,    "lib/plugin/innodb_engine.so")
+        //.addEntry(Library,    "lib/plugin/auth_socket.so")//only linux
+        .addEntry(Library,    "share/english/errmsg.sys")
+        .addEntry(Library,    "share/fill_help_tables.sql")
+        .addEntry(Library,    "share/mysql_security_commands.sql")
+        .addEntry(Library,    "share/mysql_system_tables.sql")
+        .addEntry(Library,    "share/mysql_system_tables_data.sql")
+        .addEntry(Library,    "support-files/my-default.cnf")
+        .build();
+
 
         @Override
         public boolean checkDistribution(Distribution distribution) throws IOException {
@@ -69,21 +91,63 @@ public class ArtifactStoreBuilder extends de.flapdoodle.embed.process.store.Arti
         @Override
         public IExtractedFileSet extractFileSet(Distribution distribution) throws IOException {
             File archive = getArtifact(downloadConfig, distribution);
-            File extractedFiles = preExtract(archive);
-            return super.extractFileSet(distribution);
+            File sourceFolder = preExtract(archive);
+            File destinationFolder = tempDirFactory.asFile();
+            destinationFolder.mkdir();
+            ImmutableExtractedFileSet.Builder builder = ImmutableExtractedFileSet.builder(destinationFolder);
+
+            for (FileSet.Entry entry : filesToExtract.entries()) {
+                File source = new File(sourceFolder, entry.destination());
+                File target = new File(destinationFolder, entry.destination());
+                Files.copy(source, target);
+                target.setExecutable(true);
+                if (entry.type() == Executable) {
+                    builder.executable(target);
+                } else {
+                    builder.file(entry.type(), target);
+                }
+            }
+
+//            IPackageResolver packageResolver = this.downloadConfig.getPackageResolver();
+//            FilesToExtract toExtract = new FilesToExtract(this.tempDirFactory, this.executableNaming, packageResolver.getFileSet(distribution));
+//            toExtract.generatedBaseDir();
+
+
+
+//            IPackageResolver packageResolver = this._downloadConfig.getPackageResolver();
+//            File artifact = LocalArtifactStore.getArtifact(this._downloadConfig, distribution);
+//            IExtractor extractor = Extractors.getExtractor(packageResolver.getArchiveType(distribution));
+//            IExtractedFileSet extracted = extractor.extract(this._downloadConfig, artifact, new FilesToExtract(this._tempDirFactory, this._executableNaming, packageResolver.getFileSet(distribution)));
+
+
+//            return super.extractFileSet(distribution);
+
+            return builder.build();
+        }
+
+        private File getExtractedFolderPath(File downloadedFile) {
+            String fileName = downloadedFile.getName();
+            String parentFolder = downloadedFile.getParent();
+
+            if (fileName.endsWith(".zip")) {
+                return new File(parentFolder, fileName.replace(".zip", ""));
+            } else {
+                return new File(parentFolder, fileName.replace(".tar.gz", ""));
+            }
         }
 
         private File preExtract(File downloadedFile) throws IOException {
-            File outputFolder = new File(downloadedFile.getParentFile(), "." + downloadedFile.getName());
+            File extractedFolder = new File(downloadedFile.getAbsolutePath() + "_extracted");
 
-            if (downloadedFile.getAbsolutePath().endsWith(".zip")) {
-                ArchiverFactory.createArchiver(ArchiveFormat.ZIP).extract(downloadedFile, outputFolder);
-            } else {
-                ArchiverFactory.createArchiver(ArchiveFormat.TAR, CompressionType.GZIP).extract(downloadedFile, outputFolder);
+            if (!extractedFolder.exists()) {
+                if (downloadedFile.getAbsolutePath().endsWith(".zip")) {
+                    ArchiverFactory.createArchiver(ArchiveFormat.ZIP).extract(downloadedFile, extractedFolder);
+                } else {
+                    ArchiverFactory.createArchiver(ArchiveFormat.TAR, CompressionType.GZIP).extract(downloadedFile, extractedFolder);
+                }
             }
 
-            return downloadedFile;
-
+            return extractedFolder.listFiles()[0];
         }
 
         private static File getArtifact(IDownloadConfig runtime, Distribution distribution) {
