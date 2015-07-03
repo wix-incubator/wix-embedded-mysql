d3e198d3b698db908c4b292038d40640b6de6e37
Vilius Lukosius <vilius@wix.com>
2015-07-03 13:59:11 +0300

    Test clean-up

--- wix-embedded-mysql/src/test/scala/com/wix/mysql/UsageExamplesTest.scala
+++ wix-embedded-mysql/src/test/scala/com/wix/mysql/UsageExamplesTest.scala
@@ -5,117 +5,95 @@ import java.lang
 import com.wix.mysql.EmbeddedMysql.anEmbeddedMysql
 import com.wix.mysql.ScriptResolver.classPathFile
 import com.wix.mysql.config.Charset.LATIN1
-import com.wix.mysql.config.{MysqldConfig, SchemaConfig}
+import com.wix.mysql.config.MysqldConfig.aMysqldConfig
+import com.wix.mysql.config.SchemaConfig.aSchemaConfig
 import com.wix.mysql.distribution.Version._
 import org.specs2.mutable.SpecWithJUnit
-import org.specs2.specification.Scope
+import org.specs2.specification.AfterEach
 import org.springframework.jdbc.core.JdbcTemplate
 
-import scala.reflect.{classTag, ClassTag}
-import scala.util.Try
+import scala.reflect.{ClassTag, classTag}
 
 /**
  * @author viliusl
  * @since 28/05/15
  */
-class UsageExamplesTest extends SpecWithJUnit {
+class UsageExamplesTest extends SpecWithJUnit with AfterEach {
   sequential
 
-  trait Context extends Scope {
-    var mysqld: EmbeddedMysql = _
+  var mysqld: EmbeddedMysql = _
 
-    def aSelect[T: ClassTag](onSchema: String, sql: String): T =
-      new JdbcTemplate(Datasource.aDataSource(mysqld.getConfig, SchemaConfig.aSchemaConfig(onSchema).build))
-        .queryForObject("select col1 from t1;", classTag[T].runtimeClass.asInstanceOf[Class[T]])
+  override protected def after: Any = if (mysqld != null) mysqld.stop()
 
-    def verifySchema(schema: String, withScript: String) = {
-      withScript match {
-        case "db/001_init.sql" => aSelect[lang.Long](onSchema = "aschema", sql = "select col1 from t1;") mustEqual 10
-      }
+  def aSelect[T: ClassTag](onSchema: String, sql: String): T =
+    new JdbcTemplate(Datasource.aDataSource(mysqld.getConfig, aSchemaConfig(onSchema).build))
+      .queryForObject("select col1 from t1;", classTag[T].runtimeClass.asInstanceOf[Class[T]])
+
+  def verifySchema(schema: String, withScript: String) = {
+    withScript match {
+      case "db/001_init.sql" => aSelect[lang.Long](onSchema = "aschema", sql = "select col1 from t1;") mustEqual 10
     }
   }
 
   "EmbeddedMysql can be run with " >> {
 
-    "default configuration and a single schema provided via instance builder" in new Context {
-      try {
-        mysqld = anEmbeddedMysql(v5_6_latest)
-          .addSchema("aschema", classPathFile("db/001_init.sql"))
-          .start
+    "default configuration and a single schema provided via instance builder" in {
+      mysqld = anEmbeddedMysql(v5_6_latest)
+        .addSchema("aschema", classPathFile("db/001_init.sql"))
+        .start
 
-        verifySchema("aschema", withScript = "db/001_init.sql")
-
-      } finally Try { mysqld.stop() }
+      verifySchema("aschema", withScript = "db/001_init.sql")
     }
 
-    "default configuration with custom version and a single schema provided via instance builder" in new Context {
-      try {
-        mysqld = anEmbeddedMysql(v5_5_40)
-          .addSchema("aschema", classPathFile("db/001_init.sql"))
-          .start
+    "default configuration with custom version and a single schema provided via instance builder" in {
+      mysqld = anEmbeddedMysql(v5_5_40)
+        .addSchema("aschema", classPathFile("db/001_init.sql"))
+        .start
 
-        verifySchema("aschema", withScript = "db/001_init.sql")
-
-      } finally Try { mysqld.stop() }
+      verifySchema("aschema", withScript = "db/001_init.sql")
     }
 
-    "MysqldConfig and a single schema provided via instance builder" in new Context {
-      try {
-        val config = MysqldConfig.aMysqldConfig(v5_6_latest)
-          .withPort(1120)
-          .withCharset(LATIN1)
-          .withUser("someuser", "somepassword")
-          .build()
+    "MysqldConfig and a single schema provided via instance builder" in {
+      val config = aMysqldConfig(v5_6_latest)
+        .withPort(1120)
+        .withCharset(LATIN1)
+        .withUser("someuser", "somepassword")
+        .build()
 
-        mysqld = EmbeddedMysql.anEmbeddedMysql(config)
-          .addSchema("aschema", classPathFile("db/001_init.sql"))
-          .start
+      mysqld = anEmbeddedMysql(config)
+        .addSchema("aschema", classPathFile("db/001_init.sql"))
+        .start
 
-        verifySchema("aschema", withScript = "db/001_init.sql")
-
-      } finally Try { mysqld.stop() }
+      verifySchema("aschema", withScript = "db/001_init.sql")
     }
 
-    "schema config" in new Context {
-      try {
-        val schema = SchemaConfig.aSchemaConfig("aschema")
-          .withScripts(classPathFile("db/001_init.sql"))
-          .build
+    "schema config" in {
+      val schema = aSchemaConfig("aschema")
+        .withScripts(classPathFile("db/001_init.sql"))
+        .build
 
-        mysqld = anEmbeddedMysql(v5_5_40)
-          .addSchema(schema)
-          .start
+      mysqld = anEmbeddedMysql(v5_5_40)
+        .addSchema(schema)
+        .start
 
-        verifySchema("aschema", withScript = "db/001_init.sql")
-
-      } finally Try { mysqld.stop() }
+      verifySchema("aschema", withScript = "db/001_init.sql")
     }
 
-    "multiple schemas" in new Context {
-      try {
-        mysqld = anEmbeddedMysql(v5_5_40)
-          .addSchema("aschema", classPathFile("db/001_init.sql"))
-          .addSchema("aschema2", classPathFile("db/001_init.sql"))
-          .start
+    "multiple schemas" in {
+      mysqld = anEmbeddedMysql(v5_5_40)
+        .addSchema("aschema", classPathFile("db/001_init.sql"))
+        .addSchema("aschema2", classPathFile("db/001_init.sql"))
+        .start
 
-        verifySchema("aschema", withScript = "db/001_init.sql")
-        verifySchema("aschema2", withScript = "db/001_init.sql")
-
-      } finally Try { mysqld.stop() }
+      verifySchema("aschema", withScript = "db/001_init.sql")
+      verifySchema("aschema2", withScript = "db/001_init.sql")
     }
 
-    "schema added after EmbeddedMysql start-up" in new Context {
-      try {
-        mysqld = anEmbeddedMysql(v5_6_latest).start()
-        mysqld.addSchema("aschema", classPathFile("db/001_init.sql"))
+    "schema added after EmbeddedMysql start-up" in {
+      mysqld = anEmbeddedMysql(v5_6_latest).start()
+      mysqld.addSchema("aschema", classPathFile("db/001_init.sql"))
 
-        verifySchema("aschema", withScript = "db/001_init.sql")
-
-      } finally Try { mysqld.stop() }
+      verifySchema("aschema", withScript = "db/001_init.sql")
     }
-
-//    //TODO: verify that charset provided is actual or db
-//    //TODO: verify that charset provided for instance is actual
-//    //TODO: multiple schemas
- }
+  }
 }
