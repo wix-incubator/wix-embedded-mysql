46e713f4aec5d80511a13f60bacfcc7ad545387c
Maxim Novak <maximn@wix.com>
2015-10-22 16:49:34 +0300

    stable extract dir

--- src/main/java/com/wix/mysql/config/RuntimeConfigBuilder.java
+++ src/main/java/com/wix/mysql/config/RuntimeConfigBuilder.java
@@ -1,17 +1,15 @@
 package com.wix.mysql.config;
 
 import com.wix.mysql.MysqldProcess;
+import com.wix.mysql.distribution.Version;
 import de.flapdoodle.embed.process.config.io.ProcessOutput;
-import de.flapdoodle.embed.process.io.Processors;
-import de.flapdoodle.embed.process.io.Slf4jLevel;
 import de.flapdoodle.embed.process.runtime.ICommandLinePostProcessor;
+import de.flapdoodle.embed.process.store.IArtifactStore;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import static de.flapdoodle.embed.process.io.Processors.logTo;
 import static de.flapdoodle.embed.process.io.Slf4jLevel.DEBUG;
-import static de.flapdoodle.embed.process.io.Slf4jLevel.ERROR;
-import static de.flapdoodle.embed.process.io.Slf4jLevel.INFO;
 
 /**
  * @author viliusl
@@ -24,7 +22,18 @@ public class RuntimeConfigBuilder extends de.flapdoodle.embed.process.config.Run
     public RuntimeConfigBuilder defaults() {
         processOutput().setDefault(new ProcessOutput(logTo(logger, DEBUG), logTo(logger, DEBUG), logTo(logger, DEBUG)));
         commandLinePostProcessor().setDefault(new ICommandLinePostProcessor.Noop());
-        artifactStore().setDefault(new ArtifactStoreBuilder().defaults().build());
+        artifactStore().setDefault(artifactStoreBuilderFor("mysql-default"));
         return this;
+    }
+
+    public RuntimeConfigBuilder defaults(Version version) {
+        String directoryName = String.format("mysql-%s", version.getMajorVersion());
+        defaults().artifactStore().setDefault(artifactStoreBuilderFor(directoryName));
+
+        return this;
+    }
+
+    private IArtifactStore artifactStoreBuilderFor(String directoryName) {
+        return new ArtifactStoreBuilder().defaults().defaults(directoryName).build();
     }
 }
