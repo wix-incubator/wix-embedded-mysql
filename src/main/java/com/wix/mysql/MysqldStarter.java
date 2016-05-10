97d22a1d24f823e96332db60fcb6104853ec8f11
Vilius Lukosius <vilius@wix.com>
2016-05-10 09:38:41 +0300

    Additional clean-up

--- src/main/java/com/wix/mysql/MysqldStarter.java
+++ src/main/java/com/wix/mysql/MysqldStarter.java
@@ -1,7 +1,6 @@
 package com.wix.mysql;
 
 import com.wix.mysql.config.MysqldConfig;
-import com.wix.mysql.config.RuntimeConfigBuilder;
 import de.flapdoodle.embed.process.config.IRuntimeConfig;
 import de.flapdoodle.embed.process.distribution.Distribution;
 import de.flapdoodle.embed.process.extract.IExtractedFileSet;
@@ -24,13 +23,5 @@ class MysqldStarter extends Starter<MysqldConfig, MysqldExecutable, MysqldProces
             final IRuntimeConfig runtime,
             final IExtractedFileSet exe) {
         return new MysqldExecutable(distribution, config, runtime, exe);
-    }
-
-    public static MysqldStarter instance(final IRuntimeConfig config) {
-        return new MysqldStarter(config);
-    }
-
-    public static MysqldStarter defaultInstance() {
-        return new MysqldStarter(new RuntimeConfigBuilder().defaults().build());
     }
 }
