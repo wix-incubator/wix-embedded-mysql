0d26e011fca4dca9523318b3d0cbbd4b4f97ae93
Vilius Lukosius <vilius@wix.com>
2016-11-18 09:26:07 +0200

    Another try for skipping tests for release

--- release.sh
+++ release.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-mvn -Dmaven.test.skip=true release:clean release:prepare release:perform --settings ~/.m2/settings-deploy.xml
+mvn -Darguments="-DskipTests" release:clean release:prepare release:perform --settings ~/.m2/settings-deploy.xml
