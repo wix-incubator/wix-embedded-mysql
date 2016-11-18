cec0d5ee593ab570b4759ef961db74ba9e26c3dc
Vilius Lukosius <vilius@wix.com>
2016-11-18 09:22:19 +0200

    skipping tests for relase

--- release.sh
+++ release.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-mvn release:clean release:prepare release:perform --settings ~/.m2/settings-deploy.xml
+mvn -Dmaven.test.skip=true release:clean release:prepare release:perform --settings ~/.m2/settings-deploy.xml
