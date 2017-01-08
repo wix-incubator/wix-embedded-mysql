8c87712d0cac627e513b0da3b573d784c0a18c03
Rafal Glowinski <glowinski.rafal@gmail.com>
2017-01-08 14:13:14 +0100

    Feature/issue 80 macosx sierra and https (#81)
    * Add support for latest MySQL versions and Mac OSX Sierra (#80)
    
    * Change download path to https (#80)
    
    * Stop on version 5.7.16 because 5.7.17 has different file name and it has to be addressed differently (#80)

--- src/main/java/com/wix/mysql/distribution/Version.java
+++ src/main/java/com/wix/mysql/distribution/Version.java
@@ -1,13 +1,14 @@
 package com.wix.mysql.distribution;
 
-import static java.lang.String.format;
-
 import com.wix.mysql.exceptions.UnsupportedPlatformException;
 import com.wix.mysql.utils.Utils;
 import de.flapdoodle.embed.process.distribution.IVersion;
 import de.flapdoodle.embed.process.distribution.Platform;
+
 import java.util.Arrays;
 import java.util.List;
+
+import static java.lang.String.format;
 
 public enum Version implements IVersion {
 
@@ -28,7 +29,8 @@ public enum Version implements IVersion {
     v5_7_13("5.7", 13, MacOsVersion.v10_11),
     v5_7_14("5.7", 14, MacOsVersion.v10_11),
     v5_7_15("5.7", 15, MacOsVersion.v10_11),
-    v5_7_latest(v5_7_15);
+    v5_7_16("5.7", 16, MacOsVersion.v10_11),
+    v5_7_latest(v5_7_16);
 
     private enum MacOsVersion {
         v10_6, v10_9, v10_10, v10_11;
