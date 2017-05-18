bb193ba8960986bb21286aa8e887c69ee2595d56
rezra3 <roi.ezra@intel.com>
2017-05-18 08:44:12 +0300

    added a way to override where to store the mysql artifacts

--- src/main/java/com/wix/mysql/config/DownloadConfigBuilder.java
+++ src/main/java/com/wix/mysql/config/DownloadConfigBuilder.java
@@ -4,20 +4,31 @@ import com.wix.mysql.PackagePaths;
 import de.flapdoodle.embed.process.config.store.DownloadPath;
 import de.flapdoodle.embed.process.config.store.TimeoutConfigBuilder;
 import de.flapdoodle.embed.process.extract.UUIDTempNaming;
+import de.flapdoodle.embed.process.io.directories.FixedPath;
+import de.flapdoodle.embed.process.io.directories.IDirectory;
 import de.flapdoodle.embed.process.io.directories.UserHome;
 import de.flapdoodle.embed.process.io.progress.StandardConsoleProgressListener;
+import org.apache.commons.lang3.StringUtils;
 
-public class DownloadConfigBuilder extends de.flapdoodle.embed.process.config.store.DownloadConfigBuilder {
+public class DownloadConfigBuilder extends
+    de.flapdoodle.embed.process.config.store.DownloadConfigBuilder {
+
 
     public DownloadConfigBuilder defaults() {
+        String artifactStoreProp = System.getProperty("mysql.artifact.store");
+        IDirectory artifactStore =
+            StringUtils.isEmpty(artifactStoreProp) ? new UserHome(".embedmysql")
+                : new FixedPath(artifactStoreProp);
         fileNaming().setDefault(new UUIDTempNaming());
         downloadPath().setDefault(new DownloadPath("https://dev.mysql.com/get/Downloads/"));
         progressListener().setDefault(new StandardConsoleProgressListener());
-        artifactStorePath().setDefault(new UserHome(".embedmysql"));
+        artifactStorePath().setDefault(artifactStore);
         downloadPrefix().setDefault(new DownloadPrefix("embedmysql-download"));
-        userAgent().setDefault(new UserAgent("Mozilla/5.0 (compatible; Embedded MySql; +https://github.com/wix/wix-embedded-mysql)"));
+        userAgent().setDefault(new UserAgent(
+            "Mozilla/5.0 (compatible; Embedded MySql; +https://github.com/wix/wix-embedded-mysql)"));
         packageResolver().setDefault(new PackagePaths());
-        timeoutConfig().setDefault(new TimeoutConfigBuilder().connectionTimeout(10000).readTimeout(60000).build());
+        timeoutConfig().setDefault(
+            new TimeoutConfigBuilder().connectionTimeout(10000).readTimeout(60000).build());
         return this;
     }
 }
