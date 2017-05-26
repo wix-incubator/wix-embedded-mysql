f64fe9ea3cac0804b5d7eaf0057338648d82e233
Vilius Lukošius <vilius@wix.com>
2017-05-27 00:18:51 +0300

    Fixing flaky tests, travis config, speeding-up tests (#94)

--- src/main/java/com/wix/mysql/io/TimingOutProcessExecutor.java
+++ src/main/java/com/wix/mysql/io/TimingOutProcessExecutor.java
@@ -4,24 +4,21 @@ import java.util.concurrent.TimeUnit;
 
 public class TimingOutProcessExecutor {
 
-    public static int waitFor(Process p, long timeoutNanos)
-            throws InterruptedException
-    {
+    public static int waitFor(Process p, long timeoutNanos) throws InterruptedException {
         long startTime = System.nanoTime();
         long rem = timeoutNanos;
 
         do {
             try {
                 return p.exitValue();
-            } catch(IllegalThreadStateException ex) {
-                if (rem > 0)
-                    Thread.sleep(
-                            Math.min(TimeUnit.NANOSECONDS.toMillis(rem) + 1, 100));
+            } catch (IllegalThreadStateException ex) {
+                if (rem > 0) {
+                    Thread.sleep(Math.min(TimeUnit.NANOSECONDS.toMillis(rem) + 1, 100));
+                }
             }
             rem = timeoutNanos - (System.nanoTime() - startTime);
         } while (rem > 0);
         p.destroy();
         throw new InterruptedException(String.format("Timeout of %s sec exceeded while waiting for process to complete", TimeUnit.NANOSECONDS.toSeconds(timeoutNanos)));
     }
-
 }
--- src/test/scala/com/wix/mysql/SupportedVersionsTest.scala
+++ src/test/scala/com/wix/mysql/SupportedVersionsTest.scala
@@ -1,6 +1,7 @@
 package com.wix.mysql
 
 import com.wix.mysql.EmbeddedMysql.anEmbeddedMysql
+import com.wix.mysql.config.MysqldConfig
 import com.wix.mysql.config.MysqldConfig.aMysqldConfig
 import com.wix.mysql.distribution.Version
 import com.wix.mysql.support.IntegrationTest
@@ -10,19 +11,31 @@ import org.specs2.specification.core.Fragment
 class SupportedVersionsTest extends IntegrationTest {
 
   trait Context extends Scope {
-    val log = aLogFor("root")
+    val log: Iterable[String] = aLogFor("root")
   }
 
-  Fragment.foreach(Version.values filter (_.supportsCurrentPlatform)) { version =>
+  Fragment.foreach(versionsToTest()) { version =>
 
     s"$version should work on ${System.getProperty("os.name")}" in new Context {
-      val config = aMysqldConfig(version).build
+      val config: MysqldConfig = aMysqldConfig(version).build
 
-      val mysqld = start(anEmbeddedMysql(config).addSchema("aschema"))
+      val mysqld: EmbeddedMysql = start(anEmbeddedMysql(config).addSchema("aschema"))
 
       mysqld must beAvailableOn(config, "aschema")
 
       log must not(contain("Something bad happened."))
     }
+  }
+
+  def versionsToTest(): Seq[Version] = {
+    val osSupportedVersions = Version.values filter (_.supportsCurrentPlatform)
+    val distinctMajorVersions = osSupportedVersions.foldLeft(Seq[Version]())((collected, v) => {
+      collected.find(alreadyCollected => alreadyCollected.getMajorVersion == v.getMajorVersion) match {
+        case Some(_) => collected
+        case _ => collected :+ v
+      }
+    })
+
+    distinctMajorVersions
   }
 }
