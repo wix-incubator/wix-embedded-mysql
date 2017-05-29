2a642fd6b9a7352822142145af0ae6f740488503
Vilius Lukosius <vilius@wix.com>
2017-05-29 16:28:55 +0300

    added custom cache dir

--- src/test/scala/com/wix/mysql/store/ArtifactStoreTest.scala
+++ src/test/scala/com/wix/mysql/store/ArtifactStoreTest.scala
@@ -1,17 +1,20 @@
 package com.wix.mysql.store
 
 import java.io.File
+import java.nio.file.Files
 
 import com.wix.mysql.EmbeddedMysql.anEmbeddedMysql
 import com.wix.mysql.config.ArtifactStoreConfig.anArtifactStoreConfig
 import com.wix.mysql.distribution.Version
 import com.wix.mysql.support.IntegrationTest
+import org.apache.commons.io.FileUtils.deleteDirectory
+import org.specs2.matcher.FileMatchers
 
-class ArtifactStoreTest extends IntegrationTest {
+class ArtifactStoreTest extends IntegrationTest with FileMatchers {
 
   "EmbeddedMysql artifact store" should {
 
-    "store runtime files in target/ by default" in {
+    "store runtime files in './target' by default" in {
       val mysqld = start(anEmbeddedMysql(Version.v5_7_latest))
 
       mysqld must haveSystemVariable("basedir", contain(pathFor("/target/", "/mysql-5.7-")))
@@ -24,9 +27,31 @@ class ArtifactStoreTest extends IntegrationTest {
 
       mysqld must haveSystemVariable("basedir", contain(pathFor(tempDir, "/mysql-5.7-")))
     }
+
+    "store download cache in custom location" in {
+      withTempDir { tempDir =>
+        val defaultCachePath = anArtifactStoreConfig().build().getDownloadCacheDir
+        val artifactConfig = anArtifactStoreConfig().withDownloadCacheDir(tempDir).build()
+        val mysqld = start(anEmbeddedMysql(Version.v5_7_latest, artifactConfig))
+
+        tempDir must not(beEqualToIgnoringSep(defaultCachePath))
+        new File(pathFor(tempDir, "extracted")) must beADirectory and exist
+      }
+    }
   }
 
   def pathFor(basedir: String, subdir: String): String = {
-    new File(basedir, subdir).getPath.dropRight(1)
+    new File(basedir, subdir).getPath
   }
+
+  def withTempDir[T](f: String => T): T = {
+    val tempDir = Files.createTempDirectory("embed-mysql-test").toFile
+
+    try {
+      f(tempDir.getAbsolutePath)
+    } finally {
+      deleteDirectory(tempDir)
+    }
+  }
+
 }
