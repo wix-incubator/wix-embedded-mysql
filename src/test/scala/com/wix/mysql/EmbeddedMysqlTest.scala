70473799b9e29b65c3c528dac17c31995bcaf4ca
Vilius Lukosius <vilius@wix.com>
2017-06-06 09:59:14 +0300

    Test fix

--- src/test/scala/com/wix/mysql/EmbeddedMysqlTest.scala
+++ src/test/scala/com/wix/mysql/EmbeddedMysqlTest.scala
@@ -86,7 +86,7 @@ class EmbeddedMysqlTest extends IntegrationTest {
       anUpdate(mysqld, onSchema = "aSchema", sql = "insert into t1 values(10)") must beSuccessful
       aSelect[java.lang.Long](mysqld, onSchema = "aSchema", sql = "select col1 from t1 where col1 = 10;") mustEqual 10
 
-      mysqld.reloadSchema(mysqldConfig)
+      mysqld.reloadSchema(schemaConfig)
 
       aSelect[java.lang.Long](mysqld, onSchema = "aSchema", sql = "select col1 from t1 where col1 = 10;") must
         failWith("Incorrect result size: expected 1, actual 0")
