50af80de5958332805dd34c7850a06b76a32d6bc
Vilius Lukošius <vilius@wix.com>
2018-01-28 16:01:11 +0200

    Added tests for extended characters (#119)
    support migrations with non-latin charsets

---
+++ wix-embedded-mysql/src/test/scala/com/wix/mysql/ExtendedCharsetTest.scala
@@ -0,0 +1,42 @@
+package com.wix.mysql
+
+import com.wix.mysql.EmbeddedMysql._
+import com.wix.mysql.ScriptResolver.classPathScript
+import com.wix.mysql.config.Charset
+import com.wix.mysql.config.MysqldConfig.SystemDefaults
+import com.wix.mysql.support.IntegrationTest
+import com.wix.mysql.support.IntegrationTest._
+
+class ExtendedCharsetTest extends IntegrationTest {
+
+  "EmbeddedMysql instance" should {
+
+    "support non-latin characters in login/password" in {
+      todo
+      val config = testConfigBuilder
+        .withCharset(Charset.UTF8MB4)
+        .withUser("你", "好").build
+
+      val mysqld = start(anEmbeddedMysql(config))
+
+      mysqld must beAvailableOn(3310, "你", "好", SystemDefaults.SCHEMA)
+    }
+
+    "support migration from file with extended charset" in {
+      val mysqld = start(anEmbeddedMysql(testConfigBuilder.withCharset(Charset.UTF8MB4).build)
+        .addSchema("aSchema", classPathScript("/db/004_update3.sql")))
+
+      aSelect[java.lang.String](mysqld, onSchema = "aSchema", sql = "select col2 from t1 where col1 = 1;") mustEqual "你好!"
+    }
+
+    "support inline migration with extended charset" in {
+      val mysqld = start(anEmbeddedMysql(testConfigBuilder.withCharset(Charset.UTF8MB4).build)
+        .addSchema(
+          "aSchema",
+          aMigrationWith("create table t1 (col1 INTEGER, col2 VARCHAR(10));\nINSERT INTO t1 values(1, '你好!');")))
+
+      aSelect[java.lang.String](mysqld, onSchema = "aSchema", sql = "select col2 from t1 where col1 = 1;") mustEqual "你好!"
+    }
+
+  }
+}
--- wix-embedded-mysql/src/test/scala/com/wix/mysql/MysqlTest.scala
+++ wix-embedded-mysql/src/test/scala/com/wix/mysql/MysqlTest.scala
@@ -1,6 +1,7 @@
 package com.wix.mysql
 
 import com.wix.mysql.EmbeddedMysql.anEmbeddedMysql
+import com.wix.mysql.config.Charset
 import com.wix.mysql.exceptions.CommandFailedException
 import com.wix.mysql.support.IntegrationTest
 import com.wix.mysql.support.IntegrationTest.targetTestVersion
@@ -9,7 +10,7 @@ class MysqlTest extends IntegrationTest {
 
   "mysql should emit exception info with message from 'mysql' command output'" in {
     val mysqld = start(anEmbeddedMysql(targetTestVersion))
-    val mysql = new MysqlClient(mysqld.getConfig, mysqld.executable, "information_schema")
+    val mysql = new MysqlClient(mysqld.getConfig, mysqld.executable, "information_schema", Charset.UTF8MB4)
 
     mysql.executeCommands("sele qwe from zz;") must throwA[CommandFailedException].like {
       case e: CommandFailedException => e.getMessage must contain(
