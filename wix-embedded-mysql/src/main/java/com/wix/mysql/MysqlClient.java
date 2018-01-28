fc1fe6cb9d9ec4d8a01d1957e3840562f2ba3f4e
Vilius Lukosius <vilius@wix.com>
2018-01-28 15:41:31 +0200

    Revert "a bit more play with charsets"
    This reverts commit d0a3eda42962fe80e49a15e46d50a8d37ff5b283.

--- wix-embedded-mysql/src/main/java/com/wix/mysql/MysqlClient.java
+++ wix-embedded-mysql/src/main/java/com/wix/mysql/MysqlClient.java
@@ -63,7 +63,7 @@ class MysqlClient {
                     format("--port=%s", config.getPort()),
                     schemaName}).start();
 
-            IOUtils.copy(new StringReader(sql), p.getOutputStream(), java.nio.charset.Charset.forName("UTF-8"));
+            IOUtils.copy(new StringReader(sql), p.getOutputStream(), java.nio.charset.Charset.defaultCharset());
             p.getOutputStream().close();
 
             if (p.waitFor() != 0) {
