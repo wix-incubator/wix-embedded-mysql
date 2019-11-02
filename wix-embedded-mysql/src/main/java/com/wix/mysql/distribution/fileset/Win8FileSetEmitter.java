c9d5cae5b49de8979a85423dbb154baef078e3cd
Vilius Lukosius <vilius@wix.com>
2019-11-02 20:46:19 +0200

    win

--- wix-embedded-mysql/src/main/java/com/wix/mysql/distribution/fileset/Win8FileSetEmitter.java
+++ wix-embedded-mysql/src/main/java/com/wix/mysql/distribution/fileset/Win8FileSetEmitter.java
@@ -22,10 +22,10 @@ public class Win8FileSetEmitter implements FileSetEmitter {
                 .addEntry(Library, "bin/mysql.exe")
                 .addEntry(Library, "bin/mysqladmin.exe")
                 .addEntry(Library, "share/english/errmsg.sys")
-                .addEntry(Library, "share/fill_help_tables.sql")
-                .addEntry(Library, "share/mysql_sys_schema.sql")
-                .addEntry(Library, "share/mysql_system_tables.sql")
-                .addEntry(Library, "share/mysql_system_tables_data.sql")
+//                .addEntry(Library, "share/fill_help_tables.sql")
+//                .addEntry(Library, "share/mysql_sys_schema.sql")
+//                .addEntry(Library, "share/mysql_system_tables.sql")
+//                .addEntry(Library, "share/mysql_system_tables_data.sql")
                 .build();
     }
 }
