8903e1bcd7baf9f14df64e3a06b4401e8eee6210
Kostas Pagratis <kpagratis@twitter.com>
2021-01-23 16:28:37 -0700

    Add support for MySql 8.0.21
    Add support for MySql 8.0.21

---
+++ wix-embedded-mysql/src/main/java/com/wix/mysql/distribution/fileset/OSX821FileSetEmitter.java
@@ -0,0 +1,33 @@
+package com.wix.mysql.distribution.fileset;
+
+import java.util.Objects;
+
+import com.wix.mysql.distribution.Version;
+
+import de.flapdoodle.embed.process.config.store.FileSet;
+import de.flapdoodle.embed.process.distribution.Platform;
+
+import static de.flapdoodle.embed.process.config.store.FileType.Executable;
+import static de.flapdoodle.embed.process.config.store.FileType.Library;
+import static de.flapdoodle.embed.process.distribution.Platform.OS_X;
+
+public class OSX821FileSetEmitter extends Nix implements FileSetEmitter {
+    @Override
+    public boolean matches(Platform platform, Version version) {
+        return (Platform.detect() == OS_X) && Objects.equals(version.getMajorVersion(), "8.0") && version.getMinorVersion() >= 21;
+    }
+
+    @Override
+    public FileSet emit() {
+        return FileSet.builder()
+            .addEntry(Executable, "bin/mysqld")
+            .addEntry(Library, "bin/mysql")
+            .addEntry(Library, "bin/mysqladmin")
+            .addEntry(Library, "bin/my_print_defaults")
+            .addEntry(Library, "share/english/errmsg.sys")
+            .addEntry(Library, "lib/libssl.1.1.dylib")
+            .addEntry(Library, "lib/libcrypto.1.1.dylib")
+            .addEntry(Library, "lib/libprotobuf-lite.3.11.4.dylib")
+            .build();
+    }
+}
